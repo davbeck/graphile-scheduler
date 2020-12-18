f7e383d7a6f27e8d1681d899d1d7a8d206bbc172
Matt Byrne <matt.byrne@iag.com.au>
2020-12-18 13:48:10 +1100

    Fix drop function command and tweak variable names in function
    #11

--- sql/000002.sql
+++ sql/000002.sql
@@ -1,17 +1,17 @@
-DROP FUNCTION IF EXISTS :GRAPHILE_SCHEDULER_SCHEMA.schedules_matches(ebg_toolkit_cron.schedules,timestamp with time zone);
+DROP FUNCTION IF EXISTS :GRAPHILE_SCHEDULER_SCHEMA.schedules_matches(schedule :GRAPHILE_SCHEDULER_SCHEMA.schedules, check_time TIMESTAMP WITH TIME ZONE);
 
 -- Re-create function to use the schedule.timezone to check date portions in the schedule
-CREATE OR REPLACE FUNCTION :GRAPHILE_SCHEDULER_SCHEMA.schedules_matches(schedule :GRAPHILE_SCHEDULER_SCHEMA.schedules, check_time_raw TIMESTAMP WITH TIME ZONE = NOW())
+CREATE OR REPLACE FUNCTION :GRAPHILE_SCHEDULER_SCHEMA.schedules_matches(schedule :GRAPHILE_SCHEDULER_SCHEMA.schedules, check_time TIMESTAMP WITH TIME ZONE = NOW())
 RETURNS BOOLEAN
 AS $$
 DECLARE
-  v_check_time TIMESTAMP WITH TIME ZONE;
+  v_check_time_converted_tz TIMESTAMP WITH TIME ZONE;
 BEGIN
-  v_check_time := check_time_raw AT TIME ZONE schedule.timezone;
-  RETURN EXTRACT(minute FROM v_check_time) = ANY(schedule.minute)
-     AND EXTRACT(hour FROM v_check_time) = ANY(schedule.hour)
-     AND EXTRACT(day FROM v_check_time) = ANY(schedule.day)
-     AND EXTRACT(month FROM v_check_time) = ANY(schedule.month)
-     AND EXTRACT(dow FROM v_check_time) = ANY(schedule.dow);
+  v_check_time_converted_tz := check_time AT TIME ZONE schedule.timezone;
+  RETURN EXTRACT(minute FROM v_check_time_converted_tz) = ANY(schedule.minute)
+     AND EXTRACT(hour FROM v_check_time_converted_tz) = ANY(schedule.hour)
+     AND EXTRACT(day FROM v_check_time_converted_tz) = ANY(schedule.day)
+     AND EXTRACT(month FROM v_check_time_converted_tz) = ANY(schedule.month)
+     AND EXTRACT(dow FROM v_check_time_converted_tz) = ANY(schedule.dow);
 END;
 $$ LANGUAGE plpgsql;
